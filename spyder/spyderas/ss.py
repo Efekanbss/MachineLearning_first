f0dc4b28b09b658bbd63a37d517fba28f7dbc39f
Efekan Baş <120006242+Efekanbss@users.noreply.github.com>
2024-03-30 02:10:08 +0300

    Add files via upload

---
+++ spyder/spyderas/ss.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 02:08:31 2024
+
+@author: efekanbas
+"""
+
+print("a")
---
+++ spyder/untitled1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 02:02:23 2024
+
+@author: efekanbas
+"""
+
+print("a")
